91e00d36fdd730f29bc477744726b3b7fdf9f507
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-10-22 17:09:53 +0300

    fixes

--- src/Components/navigation/Navigation.js
+++ src/Components/navigation/Navigation.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import s from "./Navigation.module.css";
+import shared from "../../Shared/sharedData";
 
 const Navigation = () => {
   return (
     <nav className={s.mainNav}>
       <NavLink
         exact
-        to="/"
+        to={shared.home}
         className={s.navLink}
         activeClassName={s.navLinkActive}
       >
@@ -15,7 +16,7 @@ const Navigation = () => {
       </NavLink>
 
       <NavLink
-        to="/movies"
+        to={shared.movies}
         className={s.navLink}
         activeClassName={s.navLinkActive}
       >
